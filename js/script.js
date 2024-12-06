42778af64a3d0f36c2c7a1a682b71ca1eac4a97f
DenisFGit <denis@gmail.com>
2024-12-06 12:46:47 +0200

    added movies

--- js/script.js
+++ js/script.js
@@ -10,9 +10,12 @@ let personalMovieDB = {
 
 let lastfilm = prompt('What film have you seen last time ?', "");
 let rate = +prompt("What rate do you give to this film ?", "");
+let lastfilm2 = prompt('What film have you seen last time ?', "");
+let rate2 = +prompt("What rate do you give to this film ?", "");
 rate = +rate;
 
 personalMovieDB.movies[lastfilm] = rate;
+personalMovieDB.movies[lastfilm2] = rate2;
 
 console.log(personalMovieDB.movies);
 
